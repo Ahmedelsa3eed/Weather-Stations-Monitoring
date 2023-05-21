5c46ddb75e8253ba96757024323530378f51542a
Yousef Saber <yousefsaber.155@gmail.com>
2023-05-21 18:30:20 +0300

    adapter and refacctor

--- statiion (2)/stationn/src/main/java/com/example/App.java
+++ statiion (2)/stationn/src/main/java/com/example/App.java
@@ -1,129 +1,48 @@
 package com.example;
 
 
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
-import org.apache.avro.io.BinaryEncoder;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.io.EncoderFactory;
-import org.apache.avro.specific.SpecificDatumWriter;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
-
 import org.apache.kafka.common.serialization.ByteArraySerializer;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Properties;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         int station_id = 1;
-        // Set up the producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
-
         String avroSchemaString = "{\"type\":\"record\",\"name\":\"WeatherData\",\"fields\":[{\"name\":\"station_id\",\"type\":\"long\"},{\"name\":\"s_no\",\"type\":\"long\"},{\"name\":\"battery_status\",\"type\":\"string\"},{\"name\":\"status_timestamp\",\"type\":\"long\"},{\"name\":\"weather\",\"type\":{\"type\":\"record\",\"name\":\"Weather\",\"fields\":[{\"name\":\"humidity\",\"type\":\"int\"},{\"name\":\"temperature\",\"type\":\"int\"},{\"name\":\"wind_speed\",\"type\":\"int\"}]}}]}";
         Schema avroSchema = new Schema.Parser().parse(avroSchemaString);
-
-        // Create Kafka producer
         KafkaProducer<String, byte[]> producer = new KafkaProducer<>(properties);
-        
-        // Generate and send messages every 2 seconds
-        App app = new App();
+        MessageHandler msqHandler = new MessageHandler(avroSchema);
         int messageCount = 1;
-        
-        while (true) {
-            GenericData.Record weatherData = app.createMessage(avroSchema, messageCount, station_id);
+        boolean work = true;
+        while (work) {
+            GenericData.Record weatherData = msqHandler.createMessage(avroSchema, messageCount, station_id);
             byte[] array;
             try {
-                array = app.genericRecordToByteArray(weatherData,avroSchema);
-                ProducerRecord<String, byte[]> record = new ProducerRecord<>("my_first",null, array);
-                Future<RecordMetadata> f  =producer.send(record);
-                Object o = f.get();
-                System.out.println("Success");
+                array = msqHandler.genericRecordToByteArray(weatherData,avroSchema);
+                ProducerRecord<String, byte[]> record = new ProducerRecord<>("try",null, array);
+                producer.send(record);
+                System.out.println("Success" + messageCount);
             } catch (Exception e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             messageCount++;
             try {
-                Thread.sleep(15000); // Wait for 2 seconds before sending the next message
+                Thread.sleep(1000); 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-        // Close the Kafka producer
-        
+        producer.close();
      }
-     private String randomBattery(){
-        Random random = new Random();
-        Double p  = random.nextDouble();
-        if( p < 0.3 ) {  //you might want to cache the Random instance
-            return "Low";
-        }
-        else if(p < .6){
-            return "High";
-        }
-        else{
-            return "Medium";
-        }
-     }
-     private GenericData.Record createMessage(Schema schema,int messageCount,int station_id){
-        GenericData.Record weatherData = new GenericData.Record(schema); 
-        Random random = new Random();
-        weatherData.put("station_id", station_id);
-        weatherData.put("s_no", messageCount);
-        String battery = randomBattery();
-        weatherData.put("battery_status", battery);
-        weatherData.put("status_timestamp", System.currentTimeMillis() / 1000L);
-
-        GenericData.Record weather = new GenericData.Record(schema.getField("weather").schema());
-        weather.put("humidity", random.nextInt(101));
-        weather.put("temperature", -20 + (40 + 20) * random.nextInt());
-        weather.put("wind_speed", random.nextInt(120 - 0 + 1) + 0);
-
-        weatherData.put("weather", weather);
-        return weatherData;
-    }
-     private static byte[] genericRecordToByteArray(GenericData.Record record, Schema schema) throws IOException {
-
-        // Create an AvroDatumWriter with the schema
-        DatumWriter<GenericData.Record> datumWriter = new SpecificDatumWriter<>(schema);
-
-        // Create a ByteArrayOutputStream
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        // Create an AvroBinaryEncoder using EncoderFactory
-        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(baos, null);
-
-        // Serialize the Avro object into the ByteArrayOutputStream
-        datumWriter.write(record, encoder);
-        encoder.flush();
-        byte[] byteArray = baos.toByteArray();
-
-        // Use the byte array as needed
-        // ...
-
-        // Close the ByteArrayOutputStream
-        baos.close();
-        return byteArray;
-    }
 }
--- statiion (2)/stationn/src/main/java/com/example/Kstream.java
+++ statiion (2)/stationn/src/main/java/com/example/Kstream.java
@@ -1,51 +1,34 @@
 package com.example;
-import io.confluent.common.utils.TestUtils;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.Decoder;
-import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.specific.SpecificDatumReader;
-import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.ForeachAction;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
-
-import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
-import java.util.regex.Pattern;
 public class Kstream {
-    static final String inputTopic = "my_first";
+    static final String inputTopic = "try";
     static final String outputTopic = "streams-wordcount-output";
+    static final String avroSchemaString = "{\"type\":\"record\",\"name\":\"WeatherData\",\"fields\":[{\"name\":\"station_id\",\"type\":\"long\"},{\"name\":\"s_no\",\"type\":\"long\"},{\"name\":\"battery_status\",\"type\":\"string\"},{\"name\":\"status_timestamp\",\"type\":\"long\"},{\"name\":\"weather\",\"type\":{\"type\":\"record\",\"name\":\"Weather\",\"fields\":[{\"name\":\"humidity\",\"type\":\"int\"},{\"name\":\"temperature\",\"type\":\"int\"},{\"name\":\"wind_speed\",\"type\":\"int\"}]}}]}";
+    static final Schema schema= new Schema.Parser().parse(avroSchemaString);;
+    static final MessageHandler messageHandler = new MessageHandler(schema);
     public static void main(String[] args) {
         final Properties streamsConfiguration = getStreamsConfiguration();
-        final StreamsBuilder builder = new StreamsBuilder();
-        createWordCountStream(builder);
+        final StreamsBuilder builder = new StreamsBuilder(); 
+        final KStream<String, byte[]> input = builder.stream(inputTopic);
+        KStream<String, String> processedStream = input.flatMap(new MyValueMapper());
+        processedStream.to(outputTopic, Produced.with(Serdes.String(), Serdes.String())); 
         final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
         streams.cleanUp();
 
-        // Now run the processing topology via `start()` to begin processing its input data.
         streams.start();
-    
-        // Add shutdown hook to respond to SIGTERM and gracefully close the Streams application.
     }
-    /**
-   * Configure the Streams application.
-   *
-   * Various Kafka Streams related settings are defined here such as the location of the target Kafka cluster to use.
-   * Additionally, you could also define Kafka Producer and Kafka Consumer settings when needed.
-   *
-   * @param bootstrapServers Kafka cluster address
-   * @return Properties getStreamsConfiguration
-   */
   static Properties getStreamsConfiguration() {
     final Properties streamsConfiguration = new Properties();
     // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
@@ -60,63 +43,23 @@ public class Kstream {
     return streamsConfiguration;
   }
 
-  static void createWordCountStream(final StreamsBuilder builder) {
-    // Construct a `KStream` from the input topic "streams-plaintext-input", where message values
-    // represent lines of text (for the sake of this example, we ignore whatever may be stored
-    // in the message keys).  The default key and value serdes will be used.
-    final KStream<String, byte[]> input = builder.stream(inputTopic);
-    input.foreach(new ForeachAction<String, byte[]>() {
-        @Override
-        public void apply(String key, byte[] value) {
-            // Perform some processing on each record
-            int humidity;
-            try {
-                humidity = getHumidaty(value);
-                System.out.println("Key: " + key + ", Value: " + humidity);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+  private static class MyValueMapper implements org.apache.kafka.streams.kstream.KeyValueMapper<String, byte[], Iterable<KeyValue<String, String>>> {
+    @Override
+    public Iterable<KeyValue<String, String>> apply(String key, byte[] value) {
+        List<KeyValue<String, String>> results = new ArrayList<>();
+        try {
+            GenericRecord wRecord = messageHandler.getWeather(value);
+            GenericRecord weather = (GenericRecord) wRecord.get("weather");
+            int humidity = (int)weather.get("humidity");
+            if (humidity > 70) {
+                System.out.println("High humidity detected");
+                String message = "High humidity detected: " + humidity + " from station " + wRecord.get("station_id") + " at time " + wRecord.get("status_timestamp");
+                results.add(new KeyValue<>(key, message));
             }
-            // Assuming you have a byte array representing the Avro serialized data
-            
-    
-            // Add your custom logic here
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    });
-    // final KTable<String, Long> wordCounts = textLines
-    //   // Split each text line, by whitespace, into words.  The text lines are the record
-    //   // values, i.e. we can ignore whatever data is in the record keys and thus invoke
-    //   // `flatMapValues()` instead of the more generic `flatMap()`.
-    //   .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
-    //   // Group the split data by word so that we can subsequently count the occurrences per word.
-    //   // This step re-keys (re-partitions) the input data, with the new record key being the words.
-    //   // Note: No need to specify explicit serdes because the resulting key and value types
-    //   // (String and String) match the application's default serdes.
-    //   .groupBy((keyIgnored, word) -> word)
-    //   // Count the occurrences of each word (record key).
-    //   .count();
-
-    // Write the `KTable<String, Long>` to the output topic.
-    // filteredStream.to(outputTopic);
-  }
-  static int getHumidaty(byte[] avroData) throws IOException{
-   
-        // Assuming you have the Avro schema for WeatherData
-        String avroSchemaString = "{\"type\":\"record\",\"name\":\"WeatherData\",\"fields\":[{\"name\":\"station_id\",\"type\":\"long\"},{\"name\":\"s_no\",\"type\":\"long\"},{\"name\":\"battery_status\",\"type\":\"string\"},{\"name\":\"status_timestamp\",\"type\":\"long\"},{\"name\":\"weather\",\"type\":{\"type\":\"record\",\"name\":\"Weather\",\"fields\":[{\"name\":\"humidity\",\"type\":\"int\"},{\"name\":\"temperature\",\"type\":\"int\"},{\"name\":\"wind_speed\",\"type\":\"int\"}]}}]}";
-        Schema schema = new Schema.Parser().parse(avroSchemaString);
-
-        // Assuming you have a byte array representing the Avro serialized data
-
-        Decoder decoder = DecoderFactory.get().binaryDecoder(avroData, null);
-
-        DatumReader<GenericRecord> datumReader = new SpecificDatumReader<>(schema);
-        GenericRecord record = datumReader.read(null, decoder);
-
-        // Extract the humidity field from the deserialized Avro object
-        GenericRecord weather = (GenericRecord) record.get("weather");
-        int humidity = (int) weather.get("humidity");
-
-        System.out.println("Humidity: " + humidity); 
-        return humidity;
+        return results;
     }
+}
 }
